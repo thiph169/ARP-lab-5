1b2760c163d12eaec385ccc977f9541bc459bb06
thiph169 <thiph169@student.liu.se>
2017-10-01 14:18:37 +0200

    First commit of lab05

---
+++ retriever/R/address.R
@@ -0,0 +1,69 @@
+#' Title Address Retriver
+#' 
+#' @description The 'address_lookup' function is a function that provides the user the address of a given
+#' a set of coordinates The funtion uses the google geocode API to retrieve the result.
+#' 
+#' @param latlong A character string that includes the latitude and the longitude.
+#'
+#' @return A dataframe containing full address and coordinates.
+#' 
+#' @examples address(latlong = "58.3978, 15.5760")
+#' 
+#' @export
+#' 
+#' 
+#' 
+address <- function(latlong = NULL){
+  require("httr")
+  #Intial
+  if(is.null(latlong) | !is.character(latlong)){
+    stop("Use character values")
+  }
+  
+  if(!grepl("[[:digit:]]|[[:punct:]]",latlong)){
+    stop("Use digits")
+  }
+  
+  #Creating query and using GET verb
+  
+  cleaner <- function(x){
+    
+    x <- gsub("([[:space:]]|[[:punct:]])\\1+", "\\1", x)
+    x <- gsub("[[:space:]]|[^[:alnum:],.+-]",",",x)
+    x <- gsub("([[:punct:]])\\1+", "\\1", x)
+    return(x)
+  }
+  
+  latlong <- cleaner(latlong)
+  url <- "https://maps.googleapis.com/maps/api/geocode/json?latlng="
+  key <- "&key=AIzaSyCd2oCbFa2bo2p29qwX9J9Qfj4WYbRkmUI"
+  url <- paste0(url,latlong,key)
+  get_res <- httr::GET(url)
+  
+  #Checking for status code
+  if(!grepl("^2",httr::status_code(get_res))){
+    stop(httr::message_for_status(get_res))
+  } else {
+    
+    
+    #Handlig result and returning
+    content_res <- httr::content(get_res)
+    geometry_res <- lapply(content_res[["results"]], "[[", "geometry")
+    coord <- as.data.frame(t(sapply(geometry_res, "[[", "location")))
+    full_adress <- unlist(lapply(content_res[["results"]], "[[", "formatted_address"))
+    
+    res_df <- data.frame("Full_address" = full_adress,
+                         "lat" = unlist(coord[,1]),
+                         "lng" = unlist(coord[,2]),
+                         stringsAsFactors = FALSE)
+    
+    rownames(res_df) <- NULL
+    
+    if(nrow(res_df) == 0){
+      stop("No match found")
+    }
+    
+    return(res_df)
+  }
+  
+}
---
+++ retriever/R/coordinates.R
@@ -0,0 +1,61 @@
+
+#' Title Coordinates Retriver
+#' 
+#' @description The coodinate function uses the google geocode API to retriever coordinates given address.
+#' 
+#' @param address A character string that includes address components
+#'
+#' @return A dataframe cointaining Full address and the coordinates
+#' 
+#' @examples coordinates(address="Mäster Mattias väg 7, Linköping, Sweden")
+#' 
+#' @export
+#' 
+#' 
+#' 
+#' 
+coordinates <- function(address = NULL){
+  require("httr")
+  #Intial
+  if(is.null(address) | !is.character(address)){
+    stop("Use character values")
+  }
+  
+  #Creating query and using GET verb
+  address <- gsub("[[:space:]]","+",address)
+  
+  url <- "https://maps.googleapis.com/maps/api/geocode/json?address="
+  key <- "&key=AIzaSyCd2oCbFa2bo2p29qwX9J9Qfj4WYbRkmUI"
+  #url <- "https://maps.googleapis.com/maps/api/geocode/json?address="
+  #key <- "&key=AIzaSyCcRPdN_sAcwiovz7EPAq31l5cFIxp-aW4"
+  url <- paste0(url,address,key)
+  get_res <- httr::GET(url)
+  
+  #Checking for status code
+  if(!grepl("^2",httr::status_code(get_res))){
+    stop(httr::message_for_status(get_res))
+  } else {
+    
+    
+    #Handlig result and returning
+    content_res <- httr::content(get_res)
+    geometry_res <- lapply(content_res[["results"]], "[[", "geometry")
+    coord <- as.data.frame(t(sapply(geometry_res, "[[", "location")))
+    full_adress <- unlist(lapply(content_res[["results"]], "[[", "formatted_address"))
+    
+    res_df <- data.frame("Full_address" = full_adress,
+                         "lat" = unlist(coord[,1]),
+                         "lng" = unlist(coord[,2]),
+                         stringsAsFactors = FALSE)
+    
+    rownames(res_df) <- NULL
+    
+    if(nrow(res_df) == 0){
+      stop("No match found")
+    }
+    
+    return(res_df)
+  }
+  
+}
+
