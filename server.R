d65b32621d6b3f4322cf810f0b8d515a680c2c52
thiph169 <32072635+thiph169@users.noreply.github.com>
2017-10-04 12:58:36 +0200

    Add files via upload

---
+++ server.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(retriever)
+
+server <- function(input, output) {
+  
+  output$print <- renderPrint(
+    coordinates(input$var) 
+  )
+  
+  output$return <- renderTable({
+    coordinates(input$var) 
+  })
+  
+}
+# Run the application
+#shinyApp(ui = ui, server = server)
+
+
